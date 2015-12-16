271210c9528246a6f570fc12a512a5c0aa3397cc
sarah garcin <sarah@g-u-i.me>
2015-12-16 19:13:00 +0100

    youtube + embed soundcloud

--- soundcloud/js/script.js
+++ soundcloud/js/script.js
@@ -15,11 +15,12 @@ $(document).ready(function(){
 			console.log(data[0]);
 			var image = data[0].artwork_url;
 			var wave = data[0].waveform_url;
+			var id = data[0].id;
 			console.log(image);
-			$(".soundcloud ul").append('<li><img src="'+image+'"/><img src="'+wave+'"/></li>');
+			var iframe = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/"+id+"&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
+			$(".soundcloud ul").append('<li><iframe width="100%" height="450" scrolling="no" frameborder="no" src="'+iframe+'"></iframe><img src="'+image+'"/><img src="'+wave+'"/></li>');
 
 		},
 		error: function() { alert('Failed!'); },
 	});
 });
-
---
+++ youtube/js/script.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+	
+// -------- Variables --------------
+	//entrez votre access token ici
+	var accessToken = "AIzaSyBIKdAoOQiu8z6Uf19Rl-yuiRiNraOQ6r0";
+	var tag = "cat";
+
+	$.ajax({ 
+		type: 'GET', 
+		url: "https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&maxResults=1&q="+tag+"&key="+accessToken, //url de récupération du json youtube
+		dataType: 'jsonp',
+		crossDomain: true, 
+		success: function (data) { 
+			console.log(data.items[0].id.videoId);
+			var id = data.items[0].id.videoId;
+			var url = "https://www.youtube.com/embed/"+id+"";
+			$(".youtube").append('<iframe width="420" height="315" src="'+url+'" frameborder="0"></iframe>');
+		},
+		error: function() { alert('Failed!'); },
+	});
+});
+
